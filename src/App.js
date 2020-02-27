c8bb2b52e527ecd014f67c798ac3d38791dc8528
pj-wise <pjwisedev@gmail.com>
2020-02-26 20:10:56 -0500

    added quarter functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react"; //import useState
 import "./App.css";
 import BottomRow from "./BottomRow";
-import HomeTouchBtn from "./components/homeTouchBtn";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -14,65 +13,73 @@ function App() {
   const [awayTeam, setAway] = useState("Tigers");
   const [awayScore, setAwayScore] = useState(0);
 
+  //quarter
+  const [quarter, setQuarter] = useState(1);
+
   //timer
-  function Timer() {
-    const [time, setTimer] = useState("30:00");
-  }
+  // function Timer() {
+  //   const [time, setTimer] = useState("30:00");
+  // }
+
+  const quarterFunc = () => {
+    if (quarter < 4) {
+      setQuarter(quarter + 1);
+    } else setQuarter(1);
+  };
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">{homeTeam}</h2>
+    <div className='container'>
+      <section className='scoreboard'>
+        <div className='topRow'>
+          <div className='home'>
+            <h2 className='home__name'>{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homeScore}</div>
+            <div className='home__score'>{homeScore}</div>
           </div>
-          <div className="timer">{time}</div>
-          <div className="away">
-            <h2 className="away__name">{awayTeam}</h2>
-            <div className="away__score">{awayScore}</div>
+          {/* <div className='timer'>{time}</div> */}
+          <div className='away'>
+            <h2 className='away__name'>{awayTeam}</h2>
+            <div className='away__score'>{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
+      <section className='buttons'>
+        <div className='homeButtons'>
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           {/* - A touchdown is worth 7 points (assume the following extra point is made) - A field goal is worth 3 points */}
 
           {/* <HomeTouchBtn homeScore={}/> */}
 
           <button
-            className="homeButtons__touchdown"
-            onClick={event => setHomeScore(homeScore + 7)}
-          >
+            className='homeButtons__touchdown'
+            onClick={event => setHomeScore(homeScore + 7)}>
             Home Touchdown
           </button>
 
           <button
-            className="homeButtons__fieldGoal"
-            onClick={event => setHomeScore(homeScore + 3)}
-          >
+            className='homeButtons__fieldGoal'
+            onClick={event => setHomeScore(homeScore + 3)}>
             Home Field Goal
           </button>
         </div>
-        <div className="awayButtons">
+        <div className='awayButtons'>
           <button
-            className="awayButtons__touchdown"
-            onClick={event => setAwayScore(awayScore + 7)}
-          >
+            className='awayButtons__touchdown'
+            onClick={event => setAwayScore(awayScore + 7)}>
             Away Touchdown
           </button>
           <button
-            className="awayButtons__fieldGoal"
-            onClick={event => setAwayScore(awayScore + 3)}
-          >
+            className='awayButtons__fieldGoal'
+            onClick={event => setAwayScore(awayScore + 3)}>
             Away Field Goal
           </button>
         </div>
+        <button className='quarter-btn' onClick={() => quarterFunc()}>
+          Change Quarter
+        </button>
       </section>
     </div>
   );
